bcf9c3fb3e5b0958fa0312cbfd31d3df62861474
Iswarya <63458679+ishu502@users.noreply.github.com>
2025-06-13 23:26:14 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -8,7 +8,7 @@ csv_file = 'C:/Users/iswarya/Downloads/onlineretail.csv'
 df = pd.read_csv(csv_file, parse_dates=['InvoiceDate'], dayfirst=True)
 
 # 3. MySQL connection (replace your actual MySQL password here)
-engine = create_engine('mysql+mysqlconnector://root:Iswarya#01@localhost:3306/salesdb')
+engine = create_engine('mysql+mysqlconnector://root:yourpassword@localhost:3306/salesdb')
 
 # 4. Upload to MySQL — replacing the table if it already exists
 try:
